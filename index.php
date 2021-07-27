90392d993ca5a0b4ae776650410a6a6c736e2581
sailendrachettri <sailendra9083@gmail.com>
2021-07-27 15:45:13 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	include "connection.php";
+	if(isset($_POST["submit"]))
+	{
+		$name=$_POST["name"];
+		$rollno=$_POST["rollno"];
+		$dept=$_POST["dept"];
+		$sem=$_POST["sem"];
+
+		$sql="INSERT INTO student_info(rollno,name,sem,department) values('$rollno','$name','$sem','$dept')";
+        
+		if(mysqli_query($conn,$sql))
+		{
+			echo "<script>alert('Recorded added sucesfully'); window.location.href='index.html';</script>";
+		}
+		else
+		{
+			echo "<script>alert('Recorded cration failed , try again'); window.location.href='index.html';</script>";		
+		} 
+
+	}
+
+?>
