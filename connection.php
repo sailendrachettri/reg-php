15af38c06644a82779662741be6aa067cd628251
sailendrachettri <sailendra9083@gmail.com>
2021-07-29 12:50:16 +0530

    adding other four pages and stylesheet

--- connection.php
+++ connection.php
@@ -15,7 +15,7 @@
     if(!$conn){
         die("Connection failed: " .mysqli_connect_error());
     }
-    else{
-        echo "connected.";
-    }
+    // else{
+    //     echo "connected.";
+    // }
 ?>  
